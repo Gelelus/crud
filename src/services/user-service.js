395916427bc47875ab9cdafeb53f175912213965
Uri <47351041+Gelelus@users.noreply.github.com>
2020-03-19 10:49:45 +0300

    test

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -3,20 +3,20 @@ const fs = require("fs");
 
 const add = async function (req) {
     if (req.name && req.age) {
-        var userName = req.name;
-        var userAge = req.age;
-        var user = { name: userName, age: userAge };
+        const userName = req.name;
+        const userAge = req.age;
+        const user = { name: userName, age: userAge };
 
-        var data = fs.readFileSync("users.json", "utf8");
-        var users = JSON.parse(data);
+        let data = fs.readFileSync("users.json", "utf8");
+        const users = JSON.parse(data);
 
         // находим максимальный id
-        var id = Math.max.apply(Math, users.map(function (o) { return o.id; }))
+        const id = Math.max.apply(Math, users.map(function (o) { return o.id; }))
         // увеличиваем его на единицу
         user.id = id + 1;
         // добавляем пользователя в массив
         users.push(user);
-        var data = JSON.stringify(users);
+        data = JSON.stringify(users);
         // перезаписываем файл с новыми данными
         fs.writeFileSync("users.json", data);
         return user;
@@ -28,12 +28,12 @@ const add = async function (req) {
 
 const get = async function (req) {
 
-    var id = req; // получаем id
-    var content = fs.readFileSync("users.json", "utf8");
-    var users = JSON.parse(content);
-    var user = null;
+    const id = req; // получаем id
+    const content = fs.readFileSync("users.json", "utf8");
+    const users = JSON.parse(content);
+    const user = null;
     // находим в массиве пользователя по id
-    for (var i = 0; i < users.length; i++) {
+    for (let i = 0; i < users.length; i++) {
         if (users[i].id == id) {
             user = users[i];
             break;
@@ -51,14 +51,14 @@ const get = async function (req) {
 const update = async function (req) {
     if (req.name && req.age && req.age) {
 
-        var userId = req.id;
-        var userName = req.name;
-        var userAge = req.age;
+        const userId = req.id;
+        const userName = req.name;
+        const userAge = req.age;
 
-        var data = fs.readFileSync("users.json", "utf8");
-        var users = JSON.parse(data);
-        var user;
-        for (var i = 0; i < users.length; i++) {
+        let data = fs.readFileSync("users.json", "utf8");
+        const users = JSON.parse(data);
+        let user;
+        for (let i = 0; i < users.length; i++) {
             if (users[i].id == userId) {
                 user = users[i];
                 break;
@@ -68,7 +68,7 @@ const update = async function (req) {
         if (user) {
             user.age = userAge;
             user.name = userName;
-            var data = JSON.stringify(users);
+            data = JSON.stringify(users);
             fs.writeFileSync("users.json", data);
             return user;
         }
@@ -84,12 +84,12 @@ const update = async function (req) {
 
 const del = async function (req) {
 
-    var id = req;
-    var data = fs.readFileSync("users.json", "utf8");
-    var users = JSON.parse(data);
-    var index = -1;
+    const id = req;
+    let data = fs.readFileSync("users.json", "utf8");
+    const users = JSON.parse(data);
+    const index = -1;
     // находим индекс пользователя в массиве
-    for (var i = 0; i < users.length; i++) {
+    for (let i = 0; i < users.length; i++) {
         if (users[i].id == id) {
             index = i;
             break;
@@ -97,8 +97,8 @@ const del = async function (req) {
     }
     if (index > -1) {
         // удаляем пользователя из массива по индексу
-        var user = users.splice(index, 1)[0];
-        var data = JSON.stringify(users);
+        const user = users.splice(index, 1)[0];
+        data = JSON.stringify(users);
         fs.writeFileSync("users.json", data);
         // отправляем удаленного пользователя
 
